bace268cdb614f5f0cbe1151c36bdc4b5cd92e71
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-04 14:25:40 -0700

    git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long c_long;
 	long long c_long_long;
 	float c_float;
-printf("size of a char: %d byte(s)\n", sizeof(c_char));
-printf("size of an int: %d byte(s)\n", sizeof(c_int));
-printf("size of a long int: %d byte(s)\n", sizeof(c_long));
-printf("size of a long long int: %d byte(s)\n", sizeof(c_long_long));
-printf("size of a float: %d byte(s)\n", sizeof(c_float));
+printf("size of a char: %lu byte(s)\n", sizeof(c_char));
+printf("size of an int: %lu byte(s)\n", sizeof(c_int));
+printf("size of a long int: %lu byte(s)\n", sizeof(c_long));
+printf("size of a long long int: %lu byte(s)\n", sizeof(c_long_long));
+printf("size of a float: %lu byte(s)\n", sizeof(c_float));
 return (0);
 }
