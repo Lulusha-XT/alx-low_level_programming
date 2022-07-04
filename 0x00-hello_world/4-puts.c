0b4fe74236b98af3e38e52c7447a83619853be44
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-04 08:11:21 -0700

    git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main(void)
 /**
  * my_function - This is a description
+ * return (0);
  */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
