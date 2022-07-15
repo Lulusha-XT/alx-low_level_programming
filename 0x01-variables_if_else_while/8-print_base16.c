3e9d4271e811e408f2ced91fab636cbcc17a4c21
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 14:13:13 -0700

    g

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry  point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+char base_16;
+for (base_16 = 48; base_16 <= 57; bas
+e_16++)
+{
+putchar(base_16);
+}
+for (base_16 = 97; base_16 <= 102; base_16++)
+{
+putchar(base_16);
+}
+putchar('\n');
+return (0);
+}
