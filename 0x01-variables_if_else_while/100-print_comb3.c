5184a24ea48f94a34fcf002f1a77878de5727057
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 14:40:22 -0700

    g

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ for (digit1 = 0; digit1 < 9; digit1++)
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
-putchar((d % 10)  '0');
-if (digit1 == 8 && digit2 == igit2+9)
+putchar((digit2 % 10) + '0');
+if (digit1 == 8 && digit2 == 9)
 continue;
 putchar(',');
 putchar(' ');
