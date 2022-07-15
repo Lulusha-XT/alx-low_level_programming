59fdb88d0a8938a9b19fffca5c07d4cccb2a490c
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 11:24:07 -0700

    g

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry  point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+int i
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
