eaaa51be1b5c5f3157d61e9d1dc0e64b40056173
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 10:57:38 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry  point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
