0f2dc6b9d671b493b056e79f269a9a1e0256223c
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 15:04:52 -0700

    g

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main - entry  point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+int digit1, digit2;
+for (digit1 = 0; digit1 < 99; digit1++)
+{
+for (digit2 = digit1; digit2 < 99; digit2++)
+{
+if (digit1 != digit2)
+{
+putchar((digit2 / 10) + 48);
+putchar((digit2 % 10) + 48);
+putchar(' ');
+putchar((digit1 / 10) + 48);
+putchar((digit1 % 10) + 48);
+if (digit2 * 100 + digit1 != 9899)
+{
+
+putchar(',');
+putchar(' ');
+
+}
+
+}
+}
+
+
+}
+
+putchar('\n');
+return (0);
+}
+
