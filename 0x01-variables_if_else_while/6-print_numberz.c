e99caf0871cdcd709442bb5bb6a0b79fa9b15ee7
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-15 13:46:35 -0700

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry  point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+int i
+for (i = 48; i < 57; i++)
+{
+putcar(i);
+}
+putchar("\n");
+return (0);
+}
