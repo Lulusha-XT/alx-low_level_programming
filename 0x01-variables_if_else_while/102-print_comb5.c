cdac2f240fdba50953d25e27e934a098724a8ac9
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-17 12:35:42 -0700

    possible combination of two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
+
 /**
-* main - entry  point
-*
-* Return: always return 0
-*/
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 99; digit1++)
+int i, j;
+
+for (i = 0; i < 100; i++){
+for(j = 0; j < 100; j++)
 {
-for (digit2 = digit1; digit2 < 99; digit2++)
-{
-if (digit1 != digit2)
-{
-putchar((digit1 / 10) + 48);
-putchar((digit1 % 10) + 48);
+if (i < j)
+
+ {
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar((digit2 / 10) + 48);
-putchar((digit2 % 10) + 48);
-
-if (digit1 * 100 + digit2 != 9899)
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
-
 putchar(',');
 putchar(' ');
-
-}
-
 }
 }
-
-
 }
-
+}
 putchar('\n');
 return (0);
 }
