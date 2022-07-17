c6056fda98e600635464fd6f0fb4c1dc612dd688
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-17 07:34:57 -0700

    printing put char using put char function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void printf_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalphabet(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+#endif
