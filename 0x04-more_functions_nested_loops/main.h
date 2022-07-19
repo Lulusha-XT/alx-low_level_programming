ce6183e89f11427e5ee7d9e275d8f60b6956e9da
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-19 14:37:44 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-vo print_square(int sizetriid);
+void print_square(int sizetriid);
 void print_angle(int size);
 void print_number(int n);
 
