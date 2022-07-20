4bd5fe0a666cce4985dde445b9938602b3605a79
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-20 13:30:26 -0700

    program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 
 {
 
-	printf("Size of a  chra: %ld byte(s)\n", sizeof(char));
+	printf("Size of a chra: %ld byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 
