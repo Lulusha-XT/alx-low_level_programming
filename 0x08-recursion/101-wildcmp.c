a56cac31c452a2d00ad544b966e9c1397707b3bd
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-20 12:23:41 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,9 +3,9 @@
 * wildcmp - compares two strings.
 * @s1: string 1.
 * @s2: string 2. It can contains a * as a special character
-* Return: 1 if re idetical,0 otherwise.
+* Return: 1 if are identical, 0 otherwise.
 */
-int wildmp(chaan cr *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 return (0);
