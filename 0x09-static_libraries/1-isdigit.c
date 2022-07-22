9cb40281adb6d8c7a27a7389981ab38a8dd619e6
Lulusha-XT <leulsileshi7@gmail.com>
2022-07-22 12:53:33 -0700

    c files

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isdigit - Check for digits 0 through 9
+*@c: int type parameter
+*Return: 1 if , ele,return 
+*/
+int_isdigi(int cdigits 0 t)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
